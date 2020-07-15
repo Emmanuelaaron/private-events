fdc3e15a336c4398e9cf5873e871515da3844c1b
Teshager <teshager8922@gmail.com>
2020-07-15 10:13:26 -0600

    Add new user form and user show template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
-  def new; end
+  before_action :set_user, only: [:show]
+  def new
+    @user = User.new
+  end
 
   def create; end
 
   def show; end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new', as: :sign_up
-  get 'users/create' 
-  get 'users/show'
+  resources :users, only: [:new,:show, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
