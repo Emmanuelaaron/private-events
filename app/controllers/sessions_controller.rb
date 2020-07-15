7d6c79c996d75e681f145666682ced25902f4666
Teshager <teshager8922@gmail.com>
2020-07-15 11:37:38 -0600

    Add navbar template to the application layout file

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,6 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(username: params['user']['username'])
-    p "this is the username: #{user.username}"
     if user
       session[:user_id] = user.id
       redirect_to user_path(user)
