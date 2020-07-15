8e7e65959afd1d0f6d6fc8f9b2a082a999fd1f36
Teshager <teshager8922@gmail.com>
2020-07-15 11:28:05 -0600

    Add a create action to the sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,17 @@
 class SessionsController < ApplicationController
   def new; end
 
-  def create; end
+  def create
+    user = User.find_by(username: params['user']['username'])
+    p "this is the username: #{user.username}"
+    if user
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash.now[:alert] = 'You are not signed up yet!'
+      render 'new'
+    end
+  end
 
   def destroy; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new', as: :log_in
-  get 'sessions/create' 
+  get 'sessions/new'
+  post 'sessions/create' , as: :log_in
   get 'sessions/destroy' , as: :log_out
   resources :users, only: [:new,:show, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
