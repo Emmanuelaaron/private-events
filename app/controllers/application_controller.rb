3fa85e4ac4caabbb8b5dc283ee14b1137bf6bbc6
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2020-07-16 00:40:34 +0300

    Create event controller and add authentication to both new and create actions to the events controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
+# rubocop: disable  Lint/UselessAssignment: Useless assignment to variable
 class ApplicationController < ActionController::Base
+  def current_user
+    current_user ||= User.find(session[:user_id])
+  end
+
+  def authenticate_user!
+    !current_user.nil?
+  end
 end
+
+# rubocop: enable  Lint/UselessAssignment: Useless assignment to variable
---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,23 @@
+class EventsController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create]
+  def new
+    @event = current_user.events.build
+  end
+
+  def create
+    @event = current_user.events.create(event_params)
+    if @event.save
+      redirect_to @event
+    else
+      render 'new'
+    end
+  end
+
+  def show; end
+
+  def index; end
+
+  def event_params
+    params.require(:event).permit(:description, :event_date)
+  end
+end
