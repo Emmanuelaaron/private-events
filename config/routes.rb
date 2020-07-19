9f78e2cc9e1232e444fc28c152ad98251df8154d
Teshager <teshager8922@gmail.com>
2020-07-19 12:30:23 -0600

    Add a sign in link to the singup page

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   post 'events/invite' ,as: :invite
   resources :users, only: [:new,:show, :create]
   resources :events, except: [:edit, :update]
+  root  to: "users#new"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
