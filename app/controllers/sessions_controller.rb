af8c7b649b6745f97582059757b49190cc795205
Teshager <teshager8922@gmail.com>
2020-07-20 03:18:36 -0600

    Add a feature test for logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
+    flash[:success] = 'You are successful logged out!'
     redirect_to sessions_new_path
   end
 
--- spec/integration/authentication_spec.rb
+++ spec/integration/authentication_spec.rb
@@ -2,13 +2,28 @@ require 'rails_helper'
 
 RSpec.describe 'authentication', type: :feature do
   let(:user) { create(:user) }
-  describe 'with valid credential' do
-    it 'it successfully loads the user page' do
-      visit '/'
-      click_on 'Login'
-      fill_in 'user_username', with: user.username
-      click_button 'Log in'
-      expect(page).to have_content('Successfuly Logged in!')
+  context 'Sign in' do
+    describe 'with valid credential' do
+      it 'it successfully loads the user page' do
+        visit root_path
+        click_on 'Login'
+        fill_in 'user_username', with: user.username
+        click_button 'Log in'
+        expect(page).to have_content('Successfuly Logged in!')
+      end
+    end
+  end
+
+  context 'Log out' do
+    describe 'when the logout button is clicked' do
+      it 'it successfully redirects to the root page' do
+        visit root_path
+        click_on 'Login'
+        fill_in 'user_username', with: user.username
+        click_button 'Log in'
+        click_on 'Log out'
+        expect(page).to have_content('You are successful logged out!')
+      end
     end
   end
 end
