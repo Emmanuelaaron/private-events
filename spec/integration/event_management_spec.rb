df0a1a681f67c984bc7b1ba7dfa115ff4bb425ff
emmanuelaaron <emmanuelisabirye9@gmail.com>
2020-07-22 23:43:08 +0300

    Add integration test to invite a user to an event

--- spec/integration/event_management_spec.rb
+++ spec/integration/event_management_spec.rb
@@ -10,6 +10,7 @@ require 'rails_helper'
 
 RSpec.describe 'Event management', type: :feature do
   let(:creator) { create(:user) }
+  let(:invitee) { create(:user) }
   let(:event) { build(:event) }
   context 'Create event' do
     describe 'with valid input' do
@@ -24,6 +25,25 @@ RSpec.describe 'Event management', type: :feature do
         fill_in 'event_event_date', with: event.event_date
         click_button 'Create Event'
         expect(page).to have_content('Event successfuly created!')
+      end
+    end
+  end
+
+  context 'Event Invitation' do
+    describe 'Inviting a another user to the created event' do
+      it 'it successfully creates an event' do
+        event.creator = creator
+        visit root_path
+        click_on 'Login'
+        fill_in 'user_username', with: creator.username
+        click_button 'Log in'
+        click_link 'Create Event'
+        fill_in 'event_description', with: event.description
+        fill_in 'event_event_date', with: event.event_date
+        click_button 'Create Event'
+        fill_in 'username', with: invitee.username
+        click_button 'Invite'
+        expect(page).to have_content(invitee.username + ' successfuly invited!')
       end
     end
   end
