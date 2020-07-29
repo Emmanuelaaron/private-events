a7e2ade3ba78bd0623b89c3d900660a838b1360d
emmanuelaaron <emmanuelisabirye9@gmail.com>
2020-07-29 18:16:47 +0300

    Remove logic from application.html.erb file to the helpers file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,12 +17,27 @@ module ApplicationHelper
   end
 
   def avatar_url(user)
-    # if user.avatar_url.present?
-    #   user.avatar_url
-    # else
-    # default_url = "#{root_url}"
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d="
-    # end
+  end
+
+  def error_flashes
+    if flash[:danger]
+      content_tag(:p, flash[:danger],
+                  class: 'alert alert-danger my-3 p-3')
+    elsif flash[:success]
+      content_tag(:p, flash[:success],
+                  class: 'alert alert-success my-3 p-3')
+    end
+  end
+
+  def logged_in_private?
+    if signed_in?
+      link_to current_user.username.capitalize,
+              current_user, class: 'navbar-brand'
+    else
+      content_tag(:p, '<%= PrivateEvents %>',
+                  class: 'avbar-brand')
+    end
   end
 end
